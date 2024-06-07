06f9192fe5fc157c64fe13f9b624a63e4d7892b8
Joaquin Cavenaghi <joaquincavenaghi@gmail.com>
2024-06-07 09:13:35 -0300

    feat: Project finish, last update

--- angular-budget-app/src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ angular-budget-app/src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-ingresar-gasto',
@@ -12,11 +13,11 @@ export class IngresarGastoComponent implements OnInit {
   formularioIncorrecto: boolean;
   textIncorrecto: string;
 
-  constructor() {
+  constructor(private _presupuestoService: PresupuestoService) {
     this.nombreGasto = '';
     this.cantidad = 0;
     this.formularioIncorrecto = false;
-    this.textIncorrecto = 'Nombre gasto o cantidad incorrecta';
+    this.textIncorrecto = '';
 
   }
 
@@ -25,6 +26,29 @@ export class IngresarGastoComponent implements OnInit {
   }
   
   agregarGasto() {
-    
+
+    if(this.cantidad > this._presupuestoService.restante){
+      this.formularioIncorrecto = true;
+      this.textIncorrecto = "Cantidad ingresada es mayor al restante";
+      return;
+    }
+    if(this.nombreGasto === '' || this.cantidad <= 0){
+      this.formularioIncorrecto = true;
+      this.textIncorrecto = "Nombre gasto o cantidad incorrecta";
+    } else {
+      //crear objeto 
+      const GASTO = {
+        nombre: this.nombreGasto,
+        cantidad: this.cantidad
+      }
+
+      // enviar objeto
+      this._presupuestoService.agregarGasto(GASTO);
+      //resetear form
+
+      this.formularioIncorrecto = false;
+      this.nombreGasto = '';
+      this.cantidad = 0;
+    }
   }
 }
